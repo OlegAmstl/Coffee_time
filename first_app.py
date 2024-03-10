9a6c47aa63f4a35553e4a1e7ec6c5b2d9419ca73
Oleg <amstl@mail.ru>
2024-03-10 17:04:57 +0700

    Form for order and template.

--- first_app.py
+++ first_app.py
@@ -1,5 +1,27 @@
 from flask import Flask, render_template, request
+import json
+import os
 
+
+def save_orders(orders, filename):
+    f = open(filename, "w")
+    json.dump(orders, f, indent=4)
+    f.close()
+    return
+
+
+def load_orders(filename):
+    if os.path.exists(filename):
+        f = open(filename, "r")
+        orders = json.load(f)
+        f.close()
+        return orders
+    else:
+        orders = []
+        return orders
+
+
+orders = load_orders('orders.json')
 app = Flask(__name__)
 
 
@@ -16,8 +38,15 @@ def greet(name='Stranger'):
 @app.route('/order', methods=('GET', 'POST'))
 def order():
     if request.method == 'POST':
-        drink = request.form['drink']
-        print("Drink: ", drink)
+        new_order = {
+            "name": request.form['name'],
+            "drink": request.form['drink'],
+            "flavor": request.form['flavor'],
+            "topping": request.form['topping']}
+        orders.append(new_order)
+        save_orders(orders, 'orders.json')
+        return render_template('print.html',
+                               new_order=new_order)
     return render_template('forms.html')
 
 
